55575494bd9070dd395d8e1c10f4f2655bd4e671
Atakan Arslan <atak.arslan@hotmail.com>
2023-01-15 18:09:47 +0300

    çözüm2

--- AlgorithmPractices/Program.cs
+++ AlgorithmPractices/Program.cs
@@ -6,11 +6,22 @@
 
         //int sayi1 = 10;
         //int sayi2 = 20;
-        int sayi1  = 10, sayi2 = 20;
+
+        //int sayi1  = 10, sayi2 = 20;
 
         //int sonuc =  sayi1 + sayi2;
         //Console.WriteLine(sayi1 + sayi2);
+
+        //Console.WriteLine(sayi1 + sayi2);
+
+        #region Çözüm 2
+        Console.WriteLine("Lütfen sayi1 değerini giriniz.");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Lütfen sayi2 değerini giriniz.");
+        int sayi2 = int.Parse(Console.ReadLine());
+
         Console.WriteLine(sayi1 + sayi2);
 
+        #endregion
     }
 }
