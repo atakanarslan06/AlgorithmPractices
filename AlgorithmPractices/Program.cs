05b457f1e967ff0b2e9be20e40719fe2ef2a410b
Atakan Arslan <atak.arslan@hotmail.com>
2023-01-15 18:01:21 +0300

    Add project files.

---
+++ AlgorithmPractices/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //iki sayının toplamını veren uıygulamayı yazınız.
+
+        //int sayi1 = 10;
+        //int sayi2 = 20;
+        int sayi1  = 10, sayi2 = 20;
+
+        //int sonuc =  sayi1 + sayi2;
+        //Console.WriteLine(sayi1 + sayi2);
+        Console.WriteLine(sayi1 + sayi2);
+
+    }
+}
