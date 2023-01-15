23919f50e9d2ce3ab38ae203dc964c3ffe3d6ce0
Atakan Arslan <atak.arslan@hotmail.com>
2023-01-15 18:37:43 +0300

    İkiSayininKarelerininToplami

--- AlgorithmPractices/Program.cs
+++ AlgorithmPractices/Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        #region Çözüm 1
         //iki sayının toplamını veren uıygulamayı yazınız.
 
         //int sayi1 = 10;
@@ -13,14 +14,30 @@
         //Console.WriteLine(sayi1 + sayi2);
 
         //Console.WriteLine(sayi1 + sayi2);
-
+        #endregion
         #region Çözüm 2
-        Console.WriteLine("Lütfen sayi1 değerini giriniz.");
-        int sayi1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Lütfen sayi2 değerini giriniz.");
-        int sayi2 = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Lütfen sayi1 değerini giriniz.");
+        //int sayi1 = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Lütfen sayi2 değerini giriniz.");
+        //int sayi2 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(sayi1 + sayi2);
+        //Console.WriteLine(sayi1 + sayi2);
+
+        #endregion
+        #region Çözüm 3
+        try
+        {
+          Console.WriteLine("Lütfen sayi1 değerini giriniz.");
+          int sayi1 = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Lütfen sayi2 değerini giriniz.");
+          int sayi2 = int.Parse(Console.ReadLine());
+
+           Console.WriteLine(sayi1 + sayi2);
+        }
+        catch 
+        {
+           Console.WriteLine("Lütfen doğru bir sayı değeri giriniz");
+        }
 
         #endregion
     }
---
+++ İkiSayininKarelerininToplami/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Kullanıcının girdiği iki sayının karelerinin toplamını veren uygulamayı yazınız.
+        #region Çözüm 1
+        //Console.WriteLine("Lütfen birinci sayıyı giriniz");
+        //int sayi1 = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Lütfen ikinci sayıyı giriniz");
+        //int sayi2 = int.Parse(Console.ReadLine());
+
+        //int sonuc = sayi1 * sayi1 + sayi2 * sayi2;
+        //Console.WriteLine(sonuc);
+        #endregion
+        #region Çözüm2
+        // Console.WriteLine("Lütfen birinci sayıyı giriniz.");
+        // int sayi1 = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Lütfen ikinci sayıyı giriniz.");
+        // int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        // //Math.Pow(2,3) : 2 nin 3. kuvveti
+        //double sonuc = Math.Pow(sayi1, 2) + Math.Pow(sayi2, 2);
+        // Console.WriteLine(sonuc);
+        #endregion
+        #region Çözüm 3
+        Console.WriteLine("Lütfen birinci ve ikinci sayıları giriniz.");
+        double sonuc = Math.Pow(int.Parse(Console.ReadLine()), 2) + Math.Pow(int.Parse(Console.ReadLine()), 2);
+       Console.WriteLine(sonuc);
+        #endregion
+    }
+}
