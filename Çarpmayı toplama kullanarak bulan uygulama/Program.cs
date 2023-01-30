4fd8686283b68ca42331d74419e22ea12e3a2739
atakanarslan06 <johndoe@example.com>
2023-01-31 02:25:28 +0300

    çarpma işlemini toplama kullanarak bulma

---
+++ Çarpmayı toplama kullanarak bulan uygulama/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Çözüm1
+        Console.WriteLine("Lütfen birinci sayıyı giriniz.");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Lütfen ikinci sayıyı giriniz.");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+        int carpmaSonucu = 0;
+        for (int i = 0; i < sayi1; i++)
+        {
+            carpmaSonucu += sayi2; 
+        }
+        System.Console.WriteLine($"{sayi1} x {sayi2} = {carpmaSonucu}");
+        #endregion Çözüm 2
+    }
+}
