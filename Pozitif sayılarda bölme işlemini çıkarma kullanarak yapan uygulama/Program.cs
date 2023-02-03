aa8920af2bef019825e8c59cf0392351f6e191c8
Atakan Arslan <atak.arslan@hotmail.com>
2023-02-03 21:31:16 +0300

    bölme islemini cıkarma kullanarak bulma

---
+++ Pozitif sayılarda bölme işlemini çıkarma kullanarak yapan uygulama/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Çözüm 1
+        //Console.WriteLine("Lütfen bölmek istediğiniz sayıyı giriniz.");
+        //int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("Lütfen böleceğiniz sayıyı giriniz.");
+        //int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        //int _sayi1 = sayi1;
+
+        //int sonuc = 0, kalan = 0;
+        ////int kalan = 0;
+        //for (int i = 0; i < _sayi1; i++)
+        //{
+        //    sayi1 -= sayi2;
+        //    sonuc++;
+
+        //    if (sayi1 < sayi2)
+        //    {
+        //        kalan = sayi1;
+        //        break;
+        //    }
+        //}
+        //Console.WriteLine($"{_sayi1} / {sayi2} = {sonuc} | Kalan = {kalan}");
+        #endregion
+        #region Çözüm 2
+
+
+        #endregion
+
+    }
+}
