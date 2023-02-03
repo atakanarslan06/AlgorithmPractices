1f7ad7eeb3e258e7bd8c3ddb961b8e0bd66d0df5
Atakan Arslan <atak.arslan@hotmail.com>
2023-02-03 21:40:28 +0300

    ç2

--- Pozitif sayılarda bölme işlemini çıkarma kullanarak yapan uygulama/Program.cs
+++ Pozitif sayılarda bölme işlemini çıkarma kullanarak yapan uygulama/Program.cs
@@ -27,8 +27,25 @@
         //Console.WriteLine($"{_sayi1} / {sayi2} = {sonuc} | Kalan = {kalan}");
         #endregion
         #region Çözüm 2
-
-
+        Console.WriteLine("Lütfen bölünen sayıyı giriniz");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Lütfen bölecek sayıyı giriniz");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+       // int sayac = sayi1;
+        int _sayi1 = sayi1;
+        int sonuc = 0, kalan = 0;
+        while (true)
+        {
+            sayi1 -= sayi2;
+            sonuc++;
+            if (sayi1 < sayi2)
+            {
+                kalan = sayi1;
+                break;
+                //sayac--;
+            }
+            Console.WriteLine($"{_sayi1} / {sayi2} = {sonuc} | Kalan = {kalan}");
+        }
         #endregion
 
     }
