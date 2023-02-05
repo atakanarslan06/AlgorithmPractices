635d525bd61f0a9990dd844fa32520bd5b9f29ca
Atakan Arslan <atak.arslan@hotmail.com>
2023-02-05 16:50:48 +0300

    Girilen sayının basamak sayısını bulan uygulama

---
+++ Girilen sayının kaç basamaklı olduğunu söyleyen uygulama/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Çözüm 1
+        //Console.WriteLine("Lütfen birinci sayıyı giriniz.");
+        //int sayi = Convert.ToInt32(Console.ReadLine());
+        //int sayac = 0;
+        //for (; sayi > 10;)
+        //{
+        //    sayi /= 10;
+        //    sayac++;   
+        //}
+        //sayac++;
+        //Console.WriteLine($"{sayi} sayısının basamak değeri {sayac}");
+        #endregion
+        #region Çözüm 2
+        //Console.WriteLine("Lütfen bir sayı giriniz");
+        //int sayi = int.Parse(Console.ReadLine());
+
+        //int _i = 0;
+        //for (int i = 0; ; ) //Sonsuz/Kısır döngüdür.
+        //{
+        //    sayi /= 10;
+        //    i++;
+        //    if (sayi < 10)
+        //        break;   
+        //}
+
+        //int sonuc = 0;
+        //for(; ; )
+        //{
+        //    sayi /= 10;
+        //    sonuc++;
+        //    if(sayi < 10)
+        //        break;
+        //}
+        //sonuc++;
+        //Console.WriteLine($"{sayi} sayısı {sonuc} basamaklıdır.");
+        #endregion
+    }
+}
