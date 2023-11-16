350d77f041cc9f00d5b3ac480f355860b2886cd9
Atakan Arslan <atak.arslan@hotmail.com>
2023-11-16 21:04:54 +0300

    LineerSearch

---
+++ LineerSearch/LineerSearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class LinearSearch
+{
+    static void Main()
+    {
+        int[] array = { 2, 4, 7, 1, 9, 6 };
+
+        // Aranacak eleman
+        int target = 7;
+
+        Console.WriteLine("Dizi:");
+        PrintArray(array);
+
+        // Arama işlemi
+        int result = LinearSearchAlgorithm(array, target);
+
+        // Sonucun ekrana yazdırılması
+        if (result != -1)
+            Console.WriteLine($"\n{target} bulundu indeks: {result}");
+        else
+            Console.WriteLine($"\n{target} bulunamadı.");
+    }
+
+    // Lineer arama algoritması
+    static int LinearSearchAlgorithm(int[] arr, int target)
+    {
+        // Dizi boyunca her elemanı kontrol et
+        for (int i = 0; i < arr.Length; i++)
+        {
+            // Hedef eleman bulunursa indeksi döndür
+            if (arr[i] == target)
+                return i;
+        }
+
+        // Eleman bulunamazsa -1 döndür
+        return -1;
+    }
+
+    // Diziyi ekrana yazdıran yardımcı fonksiyon
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+            Console.Write(arr[i] + " ");
+
+        Console.WriteLine();
+    }
+}
